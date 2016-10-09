361fa16f8fa2b4c77e511f2e9ed2946d6ab6fc09
be5invis <belleve@typeof.net>
2016-10-10 05:12:49 +0800

    cor

--- ctq.js
+++ ctq.js
@@ -36,23 +36,29 @@ function removeMids (contour) {
 }
 
 function toquad (contour) {
+	if (contour.length === 0) return [];
+	if (contour.length === 1) return [contour[0]];
 	var newcontour = [];
-	var N = contour.length;
-	for (var j = 0; j < N; j++) {
+	contour.push({
+		x: contour[0].x,
+		y: contour[0].y,
+		on: true
+	});
+	for (var j = 0; j < contour.length; j++) {
 		if (contour[j].on) {
 			newcontour.push(contour[j]);
 		} else {
 			var z1 = newcontour[newcontour.length - 1];
 			var z2 = contour[j];
-			var z3 = contour[(j + 1) % N];
-			var z4 = contour[(j + 2) % N];
+			var z3 = contour[j + 1];
+			var z4 = contour[j + 2];
 			var quadzs = cubicToQuad(z1.x, z1.y, z2.x, z2.y, z3.x, z3.y, z4.x, z4.y, 0.5);
 			var on = false;
 
 			var mx = (z1.x + z4.x) / 2;
 			var my = (z1.y + z4.y) / 2;
-			var bw = Math.abs(z4.x - z1.x);
-			var bh = Math.abs(z4.y - z1.y);
+			var bw = 0.75 * Math.abs(z4.x - z1.x);
+			var bh = 0.75 * Math.abs(z4.y - z1.y);
 			for (var k = 2; k < quadzs.length - 2; k += 2) {
 				var cx = quadzs[k];
 				var cy = quadzs[k + 1];
@@ -61,19 +67,25 @@ function toquad (contour) {
 				}
 				on = !on;
 			}
-			newcontour.push(z4);
+			newcontour.push({
+				x: z4.x,
+				y: z4.y,
+				on: true
+			});
 			j += 2;
 		}
 	}
 	return removeMids(newcontour);
 }
 
+function haspt (c) { return c && c.length > 1; }
+
 module.exports = function (font) {
 	font.CFF_ = null;
 	for (var k in font.glyf) {
 		var g = font.glyf[k];
 		if (g.contours) {
-			g.contours = g.contours.map(toquad);
+			g.contours = g.contours.map(toquad).filter(haspt);
 		}
 		g.stemH = null;
 		g.stemV = null;
